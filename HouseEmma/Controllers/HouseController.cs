429c1abf17a7d545a7189c1bd93886c6790323be
Emma :) <99496596+kodukass@users.noreply.github.com>
2023-03-30 17:38:53 +0300

    models valmis

--- HouseEmma/Controllers/HouseController.cs
+++ HouseEmma/Controllers/HouseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HouseEmma.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HouseEmma.Controllers
 {
---
+++ HouseEmma/Models/HouseDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HouseEmma.Models
+{
+    public class HouseDetailsViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int RoomCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
